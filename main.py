aa10e3d97321b890e85a6003ad6613155893dec5
amandaabedahad <amanda.abedahad@outlook.com>
2021-10-13 13:34:19 +0200

    the dataloader for train, val and test is now implemented.

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
 from transformers import AutoModel, AutoTokenizer
-from pytorch_pretrained_bert.modeling import BertForMultipleChoice
 import pandas as pd
+from classes import SentimentClassifier, SwedishSentiDataset
+import torch
+import seaborn as sns
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, TensorDataset
+
+
+def create_data_loader(df, tokenizer, max_len, batch_size):
+  ds = SwedishSentiDataset(
+    text_reviews=df.text.to_numpy(),
+    true_labels=df.sentiment.to_numpy(),
+    tokenizer=tokenizer,
+    max_len=max_len
+  )
+  return DataLoader(
+    ds,
+    batch_size=batch_size,
+    num_workers=4
+  )
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 tok = AutoTokenizer.from_pretrained('KB/bert-base-swedish-cased')
-model = AutoModel.from_pretrained('KB/bert-base-swedish-cased')
+model_name = 'KB/bert-base-swedish-cased'
 
 train_data = pd.read_csv('swedish_sentiment/train.csv')
 test_data = pd.read_csv('swedish_sentiment/test.csv')
 val_data = pd.read_csv('swedish_sentiment/dev.csv')
-print(train_data.info)
+
+seq_len = [len(i.split()) for i in train_data["text"]]
+sns.histplot(seq_len)
+plt.title("Histogram of length of sentences")
+plt.xlabel("length sentence")
+plt.savefig("plots/hist_length_sentences.png")
+max_length_sentence = 200
 
 
-def train_epoch():
-    pass
+n_classes = 2  # positive or negative
+# NETWORK TO USE WITH TRANSFER LEARNING
+net = SentimentClassifier(n_classes=n_classes,
+                          val_dropout=0.3,
+                          PRE_TRAINED_MODEL_NAME=model_name).to(device)
+
+
+batch_size = 32
+
+
+train_data_loader = create_data_loader(train_data, tok, max_length_sentence, batch_size)
+val_data_loader = create_data_loader(val_data, tok, max_length_sentence, batch_size)
+test_data_loader = create_data_loader(test_data, tok, max_length_sentence, batch_size)
+
