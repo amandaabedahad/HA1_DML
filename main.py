92be334b656d022e461dab15a0ef9fd146b5f773
amandaabedahad <amanda.abedahad@outlook.com>
2021-10-13 10:45:33 +0200

    added small things

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from transformers import AutoModel, AutoTokenizer
+import pandas as pd
 
 tok = AutoTokenizer.from_pretrained('KB/bert-base-swedish-cased')
 model = AutoModel.from_pretrained('KB/bert-base-swedish-cased')
+
+train_data = pd.read_csv('swedish_sentiment/train.csv')
+test_data = pd.read_csv('swedish_sentiment/test.csv')
+val_data = pd.read_csv('swedish_sentiment/dev.csv')
+print(train_data.info)
+
+
+def train_epoch():
+    pass
